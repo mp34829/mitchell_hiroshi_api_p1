e5782804b6417e30d6b5ba3f8df408d59ba00cd4
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-13 15:30:17 -0400

    added User service tests for practice

--- src/test/java/com/revature/p0/services/UserServiceTestSuite.java
+++ src/test/java/com/revature/p0/services/UserServiceTestSuite.java
@@ -3,10 +3,15 @@ package com.revature.p0.services;
 import com.revature.p0.documents.AppUser;
 import com.revature.p0.repos.UserRepository;
 import com.revature.p0.util.UserSession;
+import com.revature.p0.util.exceptions.AuthenticationException;
 import com.revature.p0.util.exceptions.InvalidRequestException;
 import com.revature.p0.util.exceptions.ResourcePersistenceException;
 import org.junit.*;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -115,5 +120,62 @@ public class UserServiceTestSuite {
         }
 
     }
+    @Test(expected = InvalidRequestException.class)
+    public void login_throwsException_whenGivenUsernameAsEmptyString() {
+        // Arrange
+        String emptyUsername = "";
+        //Act
+        try {
+            sut.login(emptyUsername, java.util.UUID.randomUUID().toString());
+        } finally  {
+            // Assert
+        }
+    }
 
+    @Test(expected = AuthenticationException.class)
+    public void login_throwsException_whenGivenANonexistentUser(){
+        // Arrange
+        AppUser expectedResult = null;
+        AppUser invalidUser = new AppUser("first","last","email","username","password","0");
+        when(mockUserRepo.findUserByCredentials(invalidUser.getUsername(), invalidUser.getPassword())).thenReturn(expectedResult);
+        // Act
+        sut.login(invalidUser.getUsername(), invalidUser.getPassword());
+        // Assert
+        verify(mockUserRepo, times(1)).findUserByCredentials(invalidUser.getUsername(), invalidUser.getPassword());
+    }
+
+    @Test
+    public void removeBatch_removesBatchFromBatchRegistrationsForCurrentUser_whenBatchPassedAsArgument(){
+        // Arrange
+        String batch = "batch";
+        AppUser user = new AppUser("first","last","email","username","password","0");
+        List<AppUser> batchRegistration = new ArrayList<AppUser>(Arrays.asList(user));
+
+        when(mockUserRepo.findUsersByBatch("batch")).thenReturn(batchRegistration);
+        when(mockUserSession.getCurrentUser()).thenReturn(user);
+        when(mockUserRepo.update(user, user.getUsername())).thenReturn(true);
+        // Act
+        sut.removeBatch("batch");
+        // Assert
+        Assert.assertTrue(user.getBatchRegistrations().isEmpty());
+    }
+
+    @Test
+    public void enrollBatch_addsBatchToBatchRegistrationForCurrentUser_WhenBatchPassedAsArgument(){
+        // Arrange
+
+        String batch = "batch";
+        AppUser user = new AppUser("first","last","email","username","password","0");
+        user.setBatchRegistrations(new ArrayList<String>());
+        List<String> actualResult = user.getBatchRegistrations();
+        List<String> desiredResult = new ArrayList<String>(Arrays.asList("batch"));
+
+        when(mockUserSession.getCurrentUser()).thenReturn(user);
+        when(mockUserRepo.findUserByUsername(mockUserSession.getCurrentUser().getUsername())).thenReturn(user);
+        when(mockUserRepo.update(user, user.getUsername())).thenReturn(true);
+        // Act
+        sut.enrollBatch(batch);
+        // Assert
+        Assert.assertEquals(actualResult, desiredResult);
+    }
 }
