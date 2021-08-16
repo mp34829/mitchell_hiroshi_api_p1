43fa7605af71cac2af4ddadf800bf131e5dbc372
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-16 18:14:40 -0400

    added some DTOs

---
+++ src/main/java/com/revature/p1/datasource/documents/Address.java
@@ -0,0 +1,91 @@
+package com.revature.p1.datasource.documents;
+
+import java.util.Objects;
+
+public class Address {
+    private String unitNumber;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+
+    public Address() {
+        super();
+    }
+
+    public Address(String unitNumber, String street, String city, String state, String postalCode) {
+        this.unitNumber = unitNumber;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+    }
+
+    public String getUnitNumber() {
+        return unitNumber;
+    }
+
+    public void setUnitNumber(String unitNumber) {
+        this.unitNumber = unitNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(unitNumber, address.unitNumber)
+                && Objects.equals(street, address.street)
+                && Objects.equals(city, address.city)
+                && Objects.equals(state, address.state)
+                && Objects.equals(postalCode, address.postalCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unitNumber, street, city, state, postalCode);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "unitNumber='" + unitNumber + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
+    }
+}
