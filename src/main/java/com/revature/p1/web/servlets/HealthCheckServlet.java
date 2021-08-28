d0c0c373c932a0d582351037bc361fa50bdc316d
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-28 07:56:20 -0400

    testing object mapper in authservlet

--- src/main/java/com/revature/p1/web/servlets/HealthCheckServlet.java
+++ src/main/java/com/revature/p1/web/servlets/HealthCheckServlet.java
@@ -4,13 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HealthCheckServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("{ \"status\": \"UP\" }");
         System.out.println(req.getAttribute("filtered"));
+        PrintWriter respWriter = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.getWriter().write("{ \"status\": \"UP\" }");
+
     }
 
 }
